c456e66e62f528c953c549c505ab3e596fbf463d
sqrtrev <sqrtrev@korea.ac.kr>
2020-03-23 10:00:59 +0900

    init

---
+++ AES_ENC/aes.c
@@ -0,0 +1,9 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "aes.h"
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 202R_AES_ENC
+
+- AES Encryption(written in C)
+
+- CBC Mode
+- Padding will be PCKS#7
